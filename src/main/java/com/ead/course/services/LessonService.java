627ede4e5fe28a18eb0e33790a77c8e703758e36
Carlosho <c.oliveira@nextage.com.br>
2025-05-10 12:18:53 -0300

    feat: Criação de LessonController e LessonService

--- src/main/java/com/ead/course/services/LessonService.java
+++ src/main/java/com/ead/course/services/LessonService.java
@@ -1,4 +1,12 @@
 package com.ead.course.services;
 
+import com.ead.course.dtos.LessonRecordDto;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
+import jakarta.validation.Valid;
+
+import java.util.Optional;
+
 public interface LessonService {
+    LessonModel save(LessonRecordDto lessonRecordDto, ModuleModel moduleModel);
 }
