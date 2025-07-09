f38ff1e14f666879093fa5794d88caaa7ef03fa0
Carlosho23 <carlosho23@hotmail.com>
2025-07-08 23:36:58 -0300

    feat: Implementa inscrição do usuário no curso

--- src/main/java/com/ead/course/services/impl/UserServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/UserServiceImpl.java
@@ -1,13 +1,18 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.exceptions.NotFoundException;
 import com.ead.course.models.UserModel;
 import com.ead.course.repositories.UserRepository;
 import com.ead.course.services.UserService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +28,21 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserModel save(UserModel userModel) {
         return userRepository.save(userModel);
+    }
+
+    @Transactional
+    @Override
+    public void delete(UUID userId) {
+        userRepository.deleteById(userId);
+    }
+
+    @Override
+    public Optional<UserModel> findById(UUID userId) {
+        Optional<UserModel> userModelOptional = userRepository.findById(userId);
+        if (userModelOptional.isEmpty()) {
+            throw new NotFoundException("Error: User not found.");
+        }
+
+        return userModelOptional;
     }
 }
